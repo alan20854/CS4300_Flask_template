60f39726744cc69e2f1330c0af92cfccf858a5e4
Thomas Chen <trc82@cornell.edu>
2019-04-14 04:01:38 -0400

    updated jsons

--- data/courseroster/scraper.py
+++ data/courseroster/scraper.py
@@ -33,15 +33,27 @@ def create_map(json):
   res = []
   for c in json['data']['classes']:
     data = {}
-    data['coursename'] = c['subject'] + c['catalogNbr']
+
+    if c['catalogOutcomes']:
+      outcomes=""
+      for s in c['catalogOutcomes']: 
+        outcomes+= " " + s
+    else:
+      outcomes = None
+
+    data['courseNumber'] = c['catalogNbr']
+    data['courseTitle'] = c['titleLong']
     data['description'] = c['description']
+    data['offered'] = c['catalogWhenOffered']
+    data['outcomes'] = outcomes
     data['subject'] = c['subject']
     classsec = c['enrollGroups'][0]['classSections'][0]
-    data['professor'] = set()
+    data['professor'] = []
     for meet in classsec['meetings']:
       for inst in meet['instructors']:
         name = inst['firstName'] + " " + inst['lastName']
-        data['professor'].add(name)
+        if name not in data['professor']:
+          data['professor'].append(name) 
     res.append(data)
   return res
 
@@ -55,7 +67,7 @@ def create_csv(classes):
     classes - list
   """
   with open('classes.csv','a') as g:
-    rows = ['coursename', 'subject', 'professor','description']
+    rows = ['courseNumber', 'courseTitle', 'subject', 'offered', 'professor','description', 'outcomes']
     writer = csv.DictWriter(g, fieldnames=rows)
     for r in classes:
       writer.writerow(r)
@@ -76,15 +88,29 @@ def get_subjects(roster = "SP19"):
 print("Starting data collection")
 subjects = get_subjects()
 print("Obtained subjects")
-with open('classes.csv','w') as g:
-  rows = ['coursename', 'subject', 'professor','description']
-  writer = csv.DictWriter(g, fieldnames=rows)
-  writer.writeheader()
-#subjects = ["CS", "PHYS", "INFO"]
-for x in subjects:
-  data = scrape(subject=x)
-  classes = create_map(data)
-  create_csv(classes)
+# with open('classes.csv','w') as g:
+#   rows = ['courseNumber', 'courseTitle', 'subject', 'offered', 'professor','description', 'outcomes']
+#   writer = csv.DictWriter(g, fieldnames=rows)
+#   writer.writeheader()
+# #subjects = ["CS", "PHYS", "INFO"]
+# for x in subjects:
+#   data = scrape(subject=x)
+#   classes = create_map(data)
+#   create_csv(classes)
+
+full_json = {}
+for sub in subjects:  
+  new_json = create_map(scrape(subject=sub))
+  if sub == 'CS':
+    cs_json = new_json
+
+  full_json[sub] = new_json
+
+with open('full_json.txt', 'w') as output:
+  json.dump(full_json, output)
+
+with open('cs_json.txt', 'w') as output:
+  json.dump(cs_json, output)
 
 #make graph of num professors vs major
 #make graph of coursestaht have 0 description vs major
